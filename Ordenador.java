8147010fcb35b1bdbc20e8aef0446b295b568b02
lorefior <56115031+lorefior@users.noreply.github.com>
2019-10-16 09:18:49 -0300

    Add files via upload

--- Ordenador.java
+++ Ordenador.java
@@ -1,24 +1,25 @@
 package ordenador;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class Ordenador {
 	public static void main(String[] args) {
 		Scanner leer = new Scanner(System.in);
+		System.out.println("¿Cuantos numeros desea ingresar?");
+		int c = leer.nextInt();
 		ArrayList<Integer> numeros = new ArrayList<Integer>();
-		System.out.println("ingrese numeros enteros");
-		for(int i=0; i<5; i++) {
-		int n = leer.nextInt();
+		System.out.println("Ingrese " +c + " numeros enteros!");
+		for (int i = 0; i < c; i++) {
+			int n = leer.nextInt();
 			numeros.add(n);
-	} 
-		System.out.println("ordenado de mayor a menor:");
+		}
+		System.out.println("ordenado de menor a mayor:");
 		Collections.sort(numeros);
 		numeros.forEach(System.out::println);
 
-		
-		//System.out.print(numeros);
-		
-}
+		// System.out.print(numeros);
+
+	}
 }
